6b70fd805612fc2907394be2dde00cd83df6aae9
VictorMatos <joaovictormts.087@gmail.com>
2024-10-23 22:32:45 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="">
+      <h1>Hello world</h1>
+    </div>
+  )
+}
+
